d9a3d913d02a9aff17194b8e261eb2a1de3b81e7
mono20 <h9NDBCGU>
2021-02-03 18:39:10 +0300

    update

---
+++ src/tasks/task3-2.py
@@ -0,0 +1,7 @@
+first_number = input("Введите первое число: ")
+second_number = input("Введите второе число: ")
+first_number = int(first_number)
+second_number = int(second_number)
+
+string_a = (first_number + second_number)
+print(first_number, 'плюс', second_number, 'равно', string_a)
